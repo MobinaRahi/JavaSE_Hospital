19316928ba0ab32ae28f105b75656c4efec6e003
mobina mokhbery <mobinamokhbery@gmail.com>
2025-09-19 14:15:05 +0330

    update

--- src/main/java/hospital/model/tools/DoctorMapper.java
+++ src/main/java/hospital/model/tools/DoctorMapper.java
@@ -2,16 +2,18 @@ package hospital.model.tools;
 
 import hospital.model.entity.Doctor;
 import hospital.model.entity.enums.Specialty;
+import hospital.model.service.UserService;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DoctorMapper {
 
-    public Doctor doctorMapper(ResultSet resultSet) throws SQLException {
+    public Doctor doctorMapper(ResultSet resultSet) throws Exception {
         return Doctor
                 .builder()
                 .id(resultSet.getInt("id"))
+                .user(UserService.getService().findById(resultSet.getInt("user_id")))
                 .specialty(Specialty.valueOf(resultSet.getString("specialty")))
                 .price(resultSet.getInt("price"))
                 .build();
