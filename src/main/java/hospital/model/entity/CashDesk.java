5716b0c63cfc446a510870faab121233a5c65caf
Windows 10-V1607 <saray.ghorbaninezhad.my@gmail.com>
2025-09-19 12:10:19 +0330

    صندوق و جدول دیتابیس ان اضافه شد

---
+++ src/main/java/hospital/model/entity/CashDesk.java
@@ -0,0 +1,36 @@
+package hospital.model.entity;
+
+import com.google.gson.Gson;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class CashDesk {
+
+        private int id;
+        private float cashBalance;
+        private float bankBalance;
+
+
+        public void processPayment(Payment payment) {
+            if (payment.getPayType().name().equalsIgnoreCase("CASH")) {
+                cashBalance += payment.getPrice();
+            } else if (payment.getPayType().name().equalsIgnoreCase("CARD")) {
+                bankBalance += payment.getPrice();
+            }
+        }
+
+        @Override
+        public String toString() {
+            Gson gson = new Gson();
+            return gson.toJson(this);
+        }
+    }
+
+
