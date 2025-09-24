7827e6949587061a1d58c6c25d7296631e1f8419
sarayqorbani <saray.ghorbaninezhad.my@gmail.com>
2025-09-24 18:19:31 +0330

    add paymentController by saray

--- src/main/java/hospital/controller/PaymentController.java
+++ src/main/java/hospital/controller/PaymentController.java
@@ -1,4 +1,40 @@
 package hospital.controller;
 
-public class PaymentController {
+import hospital.model.entity.Payment;
+import hospital.model.entity.enums.PayFor;
+import hospital.model.entity.enums.PayType;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import lombok.extern.log4j.Log4j;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.ResourceBundle;
+@Log4j
+public class PaymentController implements Initializable{
+    @FXML
+    private TextField idText,priceText;
+    @FXML
+    private DatePicker DatePicker;
+    @FXML
+    private ComboBox<PayType> payTypeCombo;
+    @FXML
+    private ComboBox<PayFor> payForCombo;
+    @FXML
+    private Button saveButton,editButton,deleteButton;
+    @FXML
+    private TableView<Payment> paymentTable;
+    @FXML
+    private TableColumn<Payment,Integer> idColumn,priceColumn;
+    @FXML
+    private TableColumn<Payment, LocalDateTime>dateColumn;
+    @FXML
+    private TableColumn<Payment,PayType> payTypeColumn;
+    @FXML
+    private TableColumn<Payment,PayFor> payForColumn;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
