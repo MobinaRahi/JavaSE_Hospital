e33a30b81dff644fe2dbbd2bbf87f23768b9ad5c
Windows 10-V1607 <saray.ghorbaninezhad.my@gmail.com>
2025-09-27 01:50:32 +0330

    edit by saray

--- src/main/java/hospital/controller/PaymentController.java
+++ src/main/java/hospital/controller/PaymentController.java
@@ -4,14 +4,18 @@ import hospital.model.entity.Payment;
 import hospital.model.entity.enums.PayFor;
 import hospital.model.entity.enums.PayType;
 import hospital.model.service.PaymentService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import lombok.extern.log4j.Log4j2;
 
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.ResourceBundle;
 @Log4j2
@@ -38,9 +42,67 @@ public class PaymentController implements Initializable{
     private TableColumn<Payment,PayFor> payForColumn;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            resetForm();
+
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Error Loading Data", ButtonType.OK);
+            alert.show();
+        }
+        saveButton.setOnAction(event -> {
+            try {
+                Payment payment =
+                        Payment
+                                .builder()
+                                .payType(payTypeCombo.getSelectionModel().getSelectedItem())
+                                .payDateTime(DatePicker.getValue().atTime(LocalTime.now()))
+                                .price(Integer.parseInt(priceText.getText()))
+                                .payFor(payForCombo.getSelectionModel().getSelectedItem())
+                                .build();
+                PaymentService.getService().save(payment);
+                log.info("payment Saved successFully");
+            } catch (Exception e) {
+                log.error("payment save Failed" + e.getMessage());
+            }
+        });
+
+        editButton.setOnAction(event -> {
+            try {
+                Payment selectedPayment = paymentTable.getSelectionModel().getSelectedItem();
+                if (selectedPayment != null) {
+                    selectedPayment.setPayType(payTypeCombo.getSelectionModel().getSelectedItem());
+                    selectedPayment.setPayDateTime(DatePicker.getValue().atTime(LocalTime.now()));
+                    selectedPayment.setPrice(Integer.parseInt(priceText.getText()));
+                    selectedPayment.setPayFor(payForCombo.getSelectionModel().getSelectedItem());
+
+                    PaymentService.getService().edit(selectedPayment);
+                    log.info("payment Updated successfully");
+                } else {
+                    log.warn("No payment selected for update");
+                }
+            } catch (Exception e) {
+                log.error("payment update Failed: " + e.getMessage());
+            }
+        });
+        deleteButton.setOnAction((event) -> {
+            try {
+                PaymentService.getService().delete(Integer.parseInt(idText.getText()));
+                log.info("Patient Delete Successfully");
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Deleted Successfully\n" + idText.getText(), ButtonType.OK);
+                alert.show();
+                resetForm();
+            } catch (Exception e) {
+                log.error("Payment Delete Failed " + e.getMessage());
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Payment Delete Failed " + e.getMessage(), ButtonType.OK);
+                alert.show();
+            }
+        });
+        paymentTable.setOnMouseReleased((event) -> selectFromTable());
+        paymentTable.setOnKeyReleased((event) -> selectFromTable());
+
 
     }
-    private void resetForm(){
+    private void resetForm() throws Exception{
         idText.clear();
         for( PayType payType:PayType.values()){
             payTypeCombo.getItems().add(payType);
@@ -52,7 +114,34 @@ public class PaymentController implements Initializable{
             payForCombo.getItems().add(payFor);
         }
         priceText.clear();
-      //  showDateOnTable(PaymentService.getService().findAll());
+        showDataOnTable(PaymentService.getService().findAll());
     }
-    private void showDateOnTable(List<Payment> paymentList){}
+    private void showDataOnTable(List<Payment> paymentList) {
+        ObservableList<Payment> observableList = FXCollections.observableList(paymentList);
+
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        payTypeColumn.setCellValueFactory(new PropertyValueFactory<>("payType"));
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("payDateTime"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        payForColumn.setCellValueFactory(new PropertyValueFactory<>("payFor"));
+
+        paymentTable.setItems(observableList);
+    }
+
+
+    private void selectFromTable() {
+        try {
+            Payment payment = paymentTable.getSelectionModel().getSelectedItem();
+            idText.setText(String.valueOf(payment.getId()));
+            payTypeCombo.getSelectionModel().select(payment.getPayType());
+            DatePicker.setValue(payment.getPayDateTime().toLocalDate());
+            priceText.setText(String.valueOf(payment.getPrice()));
+            payForCombo.getSelectionModel().select(payment.getPayFor());
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Error Loading Data !!!", ButtonType.OK);
+            alert.show();
+        }
+    }
 }
+
+
